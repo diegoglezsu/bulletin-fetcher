ca237b29fcb785deb798c0ad8ce1ad1c14620352
Diego González Suárez <gonzalezsdiego@uniovi.es>
2026-06-25 22:40:18 +0200

    chore: bump version to 0.5.8

--- src/bulletin/__init__.py
+++ src/bulletin/__init__.py
@@ -1,6 +1,10 @@
-__version__ = "0.5.7"
-__author__ = "Diego González Suárez"
-__email__ = "gonzalezsdiego@uniovi.es"
+__version__ = "0.5.8"
+__author__ = [
+    "Diego González Suárez",
+    "Noelia Rico",
+    "Irene Díaz"
+]
+__email__ = ["gonzalezsdiego@uniovi.es", "noeliarico@uniovi.es", "sirene@uniovi.es"]
 __license__ = "MIT"
 
 from . import eurlex
